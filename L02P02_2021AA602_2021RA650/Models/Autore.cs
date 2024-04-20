13441327615035c699693107df1b34ea6ec13f6b
arrevillagamario <marrevillaga03@gmail.com>
2024-04-20 14:23:18 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021AA602_2021RA650/Models/Autore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L02P02_2021AA602_2021RA650.Models;
+
+public partial class Autore
+{
+    public int Id { get; set; }
+
+    public string? Autor { get; set; }
+
+    public virtual ICollection<Libro> Libros { get; set; } = new List<Libro>();
+}
---
+++ L02P02_2021AA602_2021RA650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021AA602_2021RA650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
