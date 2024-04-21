4064dd42a6b59160d16fe6329a4a47f16880885b
arrevillagamario <marrevillaga03@gmail.com>
2024-04-20 20:20:38 -0600

    commit4

---
+++ L02P02_2021AA602_2021RA650/Controllers/ConfirmacionController.cs
@@ -0,0 +1,23 @@
+﻿using L02P02_2021AA602_2021RA650.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2021AA602_2021RA650.Controllers
+{
+
+    public class ConfirmacionController : Controller
+    {
+        private readonly LibreriaDbContext _context;
+
+        public ConfirmacionController(LibreriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var libreriaDbContext = _context.ComentariosLibros.Include(c => c.IdLibroNavigation);
+            return View(await libreriaDbContext.ToListAsync());
+        }
+    }
+}
